535ba61422cf62fe0cc080d2753cd08b42c28dbd
TameerAmer <tameergti@gmail.com>
2025-09-04 14:41:34 +0300

    Allure format fix

--- conftest.py
+++ conftest.py
@@ -2,21 +2,27 @@ import os
 import allure
 import pytest
 
+from dotenv import load_dotenv
+import os
+
+# Load variables from .env.compose
+load_dotenv(dotenv_path=".env.compose")
+
 def pytest_configure(config):
-    """
-    Called once before running tests.
-    Here we write environment properties for Allure.
-    """
     os.makedirs("allure-results", exist_ok=True)
     with open("allure-results/environment.properties", "w") as f:
-        f.write(f"Browser={os.environ.get('BROWSER')}\n")
-        f.write(f"Resolution={os.environ.get('SCREEN_WIDTH')}x{os.environ.get('SCREEN_HEIGHT')}\n")
-        f.write(f"Environment={os.environ.get('ENVIRONMENT')}\n")
-        f.write(f"YOLO_VERSION={os.environ.get('YOLO_VERSION')}\n")
-        f.write(f"YOLO_IMG_TAG={os.environ.get('YOLO_IMG_TAG')}\n")
-        f.write(f"OLLAMA_VERSION={os.environ.get('OLLAMA_VERSION')}\n")
-        f.write(f"OLLAMA_UI_IMG_TAG={os.environ.get('OLLAMA_UI_IMG_TAG')}\n")
-        f.write(f"POSTGRES_VERSION={os.environ.get('POSTGRES_VERSION')}\n")
+        f.write("Test.Framework=Selenium+Pytest\n")
+        f.write("CI.Platform=GitHub Actions\n")
+        f.write("Test.Types=UI Automation\n")
+        f.write("Browsers=Chrome, Firefox\n")
+        f.write("Resolutions=Desktop (1920x1080), Laptop (1366x768), Tablet (1024x768), Mobile (375x667)\n")
+        f.write(f"Target.URL={os.environ.get('OLLAMA_URL')}\n")
+        f.write(f"Ollama.Version={os.environ.get('OLLAMA_VERSION')}\n")
+        f.write(f"Ollama.Image={os.environ.get('OLLAMA_UI_IMG_TAG')}\n")
+        f.write(f"YOLO.Version={os.environ.get('YOLO_VERSION')}\n")
+        f.write(f"YOLO.Image={os.environ.get('YOLO_IMG_TAG')}\n")
+        f.write(f"Postgres.Version={os.environ.get('POSTGRES_VERSION')}\n")
+
 
 
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
